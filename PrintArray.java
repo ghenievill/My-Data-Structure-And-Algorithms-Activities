7c383c86fcc0b6c869a728a84ee3d734b89b7b5a
ghenievill <ghenievillanueva05@gmail.com>
2024-10-07 20:07:47 +0800

    Create PrintArray.java

---
+++ PrintArray.java
@@ -0,0 +1,11 @@
+public class Array {
+	public static void main(String [ ] args) {
+		
+		int [ ] array = {5, 27, 15};
+		for ( int i=0; i<3;  i++ ) {
+		
+		System.out.println (array [i]);
+		}
+		
+	}
+}
