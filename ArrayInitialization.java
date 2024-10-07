f950dfe5944d21d418a33b8579d327709352d98f
ghenievill <ghenievillanueva05@gmail.com>
2024-10-07 20:05:33 +0800

    Update ArrayInitialization.java

--- ArrayInitialization.java
+++ ArrayInitialization.java
@@ -2,10 +2,7 @@ public class Array {
 	public static void main(String [ ] args) {
 		
 		int [ ] array = {3, 22, 15};
-		for ( int i=0; i<3;  i++ ) {
 		
-		System.out.println (array [i]);
-		}
 		
-	}
+ }
 }
